6e61f625a6f87f41eadd2ded5a8254ece749685e
Yellow-Shadow <58462821+Yellow-Shadow@users.noreply.github.com>
2019-12-27 01:35:27 +0800

    Update JQtest.js

--- JQtest.js
+++ JQtest.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-  $("button").click(function(){
-    $("ul").animate({
+  $("#IWTK").click(function(){
+    $("#ul_1").animate({
+      height: 'toggle'
+    });
+  });
+});
+$(document).ready(function(){
+  $("#VTNI").click(function(){
+    $("#ul_2").animate({
+      height: 'toggle'
+    });
+  });
+});
+$(document).ready(function(){
+  $("#VTNL").click(function(){
+    $("#ul_3").animate({
       height: 'toggle'
     });
   });
